28948bae56731a4913139eb941329ebc55dcd995
breejudith <31835568+breejudith@users.noreply.github.com>
2017-11-06 17:48:30 -0800

    First commit

---
+++ formhandler/app/scripts/src/formhandler.js
@@ -0,0 +1,37 @@
+((window => {
+  const App = window.App || {};
+  const $ = window.jQuery;
+
+  class FormHandler {
+    constructor(selector) {
+      if (!selector) {
+        throw new Error('No selector provided');
+      }
+
+      this.$formElement = $(selector);
+      if (this.$formElement.length === 0) {
+        throw new Error(`Could not find element with selector: ${selector}`);
+      }
+    }
+
+    addSubmitHandler(fn) {
+      console.log('Setting submit handler for form');
+      this.$formElement.on('submit', function (event) {
+        event.preventDefault();
+
+        const data = {};
+        $(this).serializeArray().forEach(item => {
+          data[item.name] = item.value;
+          console.log(`${item.name} is ${item.value}`);
+        });
+        console.log(data);
+        fn(data);
+        this.reset();
+        this.elements[0].focus();
+      });
+    }
+  }
+
+  App.FormHandler = FormHandler;
+  window.App = App;
+}))(window);
---
+++ formhandler/app/scripts/src/main.js
@@ -0,0 +1,6 @@
+import FormHandler from 'formhandler.js';
+
+let FORM_SELECTOR = '[data-coffee-order="form"]';
+let formHandler = new FormHandler(FORM_SELECTOR);
+formHandler.addSubmitHandler(() =>{
+});
